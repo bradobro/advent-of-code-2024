a5e60d77b736698efcf869a666fd2afb370b0d4b
Brad Olson <brad.olson@movedbylight.com>
2024-12-04 13:54:54 -0500

    begin: day 5 stubbed

--- deno/cli.ts
+++ deno/cli.ts
@@ -1,8 +1,9 @@
 import { Day04 } from "./day_04.ts";
+import { Day05 } from "./day_05.ts";
 
 export async function cli() {
   console.log(`Running ${Deno.args}`);
-  const puz = new Day04();
+  const puz = new Day05();
   const result = await puz.solve();
   console.log(result);
 }
---
+++ deno/day_05.ts
@@ -0,0 +1,26 @@
+import { assertEquals } from "@std/assert";
+import { fileLines } from "./lib.ts";
+import { Puzzle, Results } from "./Puzzle.ts";
+
+export class Day05 extends Puzzle<Results> {
+  constructor() {
+    super(1);
+  }
+
+  async load() {
+    let lineCount = 0;
+    const lines: string[] = [];
+    for await (const line of fileLines(this.dataFilePath)) {
+      lines.push(line);
+      lineCount++;
+    }
+    assertEquals(lineCount, lines.length);
+    return { lines, lineCount };
+  }
+
+  override async solve(): Promise<Results> {
+    const { lineCount, lines } = await this.load();
+    const results = { lineCount, lines: lines.length };
+    return { day: 5, hash: await this.hash(results), results };
+  }
+}
