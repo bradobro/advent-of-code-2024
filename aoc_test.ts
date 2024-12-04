f3d89dd9c667f6115d359e7de0a8186abef76020
Brad Olson <brad.olson@movedbylight.com>
2024-12-04 12:43:07 -0500

    chore: remove obsolete puzzle1 puzzle2

--- aoc_test.ts
+++ aoc_test.ts
@@ -2,13 +2,9 @@ import { Day01 } from "./deno/day_01.ts";
 import { Day02 } from "./deno/day_02.ts";
 import { Day03 } from "./deno/day_03.ts";
 import { Day04 } from "./deno/day_04.ts";
-import { Puzzle01 } from "./deno/puzzle_01.ts";
-import { Puzzle02 } from "./deno/puzzle_02.ts";
 
 Deno.test(async function runAllDays() {
   await new Day01().solve();
-  // await new Puzzle01().solve();
-  // await new Puzzle02().solve();
   await new Day02().solve();
   await new Day03().solve();
   await new Day04().solve();
