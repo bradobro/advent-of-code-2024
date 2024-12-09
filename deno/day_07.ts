abab3c5effb7a3a4c439d91af79a66cc1dab9cd5
Brad Olson <brad.olson@movedbylight.com>
2024-12-09 11:23:47 -0500

    feat: adding combiner utility

--- deno/day_07.ts
+++ deno/day_07.ts
@@ -1,6 +1,59 @@
-import { assertEquals } from "@std/assert";
+import {
+  assertEquals,
+  assertGreater,
+  assertGreaterOrEqual,
+  assertLess,
+} from "@std/assert";
 import { fileLines } from "./lib.ts";
 import { Puzzle, Results } from "./Puzzle.ts";
+
+/**
+ * @param count how many you want
+ * @param choices the possible things to chose from
+ * @param combo ord of the combination a number from 0...choices.length**count
+ */
+export function combination<T>(
+  count: number,
+  choices: T[],
+  combo: number,
+): T[] {
+  const result: T[] = []; // Least significant first (like LSB)
+
+  const base = choices.length;
+  const maxI = base ** count;
+  assertGreaterOrEqual(combo, 0, `instance must be 0..${maxI}`);
+  assertLess(combo, maxI, `instance must be 0..${maxI}`);
+
+  // strip off choices using the convert-to-base algorithm
+  // formatInt might be faster
+  let rest = combo;
+  for (let i = 0; i < count; i++) {
+    const digit = rest % base;
+    result.push(choices[digit]);
+    rest = (rest / base) | 0; // bitwise integer division https://www.basedash.com/blog/how-to-do-integer-division-in-javascript
+  }
+
+  return result;
+}
+
+export type IntOp = (a: number, b: number) => number;
+
+export class Row {
+  constructor(public answer: number, public factors: number[]) {}
+
+  static parseString(trimmed: string): Row {
+    const colonSplit = trimmed.split(":");
+    assertEquals(
+      colonSplit.length,
+      2,
+      `expecting one colon in the line: ${trimmed}`,
+    );
+    const answer = parseInt(colonSplit[0]);
+    const factors = colonSplit[1].trim().split(/ +/).map((s) => parseInt(s));
+    const result = new Row(answer, factors);
+    return result;
+  }
+}
 
 export class Day07 extends Puzzle<Results> {
   constructor() {
@@ -9,18 +62,21 @@ export class Day07 extends Puzzle<Results> {
 
   async load() {
     let lineCount = 0;
-    const lines: string[] = [];
+    const rows: Row[] = [];
     for await (const line of fileLines(this.dataFilePath)) {
-      lines.push(line);
+      const trimmed = line.trim();
+      if (trimmed === "") continue; // skip blank lines
+      rows.push(Row.parseString(line));
       lineCount++;
     }
-    assertEquals(lineCount, lines.length);
-    return { lines, lineCount };
+    assertEquals(lineCount, rows.length);
+    return { rows, lineCount };
   }
 
   override async solve(): Promise<Results> {
-    const { lineCount, lines } = await this.load();
-    const results = { lineCount, lines: lines.length };
+    const { lineCount, rows } = await this.load();
+    const results = { lineCount, lines: rows.length };
+    console.debug(rows.slice(0, 5));
     return { day: this.dayNumber, hash: await this.hash(results), results };
   }
 }
---
+++ deno/day_07_test.ts
@@ -0,0 +1,84 @@
+import { describe, it } from "jsr:@std/testing/bdd";
+import { expect } from "jsr:@std/expect";
+import { beforeMeAfter, meRest } from "./lib.ts";
+import { combination, IntOp, Row } from "./day_07.ts";
+import { DIGEST_ALGORITHM_NAMES } from "@std/crypto/crypto";
+import { assertThrows } from "@std/assert/throws";
+
+const raw1 = [
+  "25056746772: 4 47 136 21 79 49",
+  "3790126111: 371 8 10 125 325 788",
+  "37542888: 3 977 944 5",
+  "785818128: 1 8 5 9 4 73 5 7 860 7",
+  "19035366: 37 41 1 42 555 9 366",
+];
+
+const data1 = [
+  new Row(25056746772, [4, 47, 136, 21, 79, 49]),
+  new Row(3790126111, [371, 8, 10, 125, 325, 788]),
+  new Row(37542888, [3, 977, 944, 5]),
+  new Row(785818128, [1, 8, 5, 9, 4, 73, 5, 7, 860, 7]),
+  new Row(19035366, [37, 41, 1, 42, 555, 9, 366]),
+];
+
+describe("Row object", () => {
+  it("parses row strings", () => {
+    const parsed1 = raw1.map(Row.parseString);
+    expect(parsed1).toEqual(data1);
+  });
+});
+
+describe(" combiner", () => {
+  it("produces combinations aligned with combo id", () => {
+    const choices = ["0", "1"];
+    // 1 digit
+    expect(combination(1, choices, 0).join("")).toEqual("0");
+    expect(combination(1, choices, 1).join("")).toEqual("1");
+
+    // 2 digits
+    expect(combination(2, choices, 0).join("")).toEqual("00");
+    expect(combination(2, choices, 1).join("")).toEqual("10");
+    expect(combination(2, choices, 2).join("")).toEqual("01");
+    expect(combination(2, choices, 3).join("")).toEqual("11");
+    assertThrows(() => combination(2, choices, 4).join(""));
+    assertThrows(() => combination(2, choices, -1).join(""));
+
+    // 10 digits spot check
+    expect(combination(10, choices, 0).join("")).toEqual("0000000000");
+    expect(combination(10, choices, 1).join("")).toEqual("1000000000");
+    expect(combination(10, choices, 2).join("")).toEqual("0100000000");
+    expect(combination(10, choices, 3).join("")).toEqual("1100000000");
+
+    expect(combination(10, choices, 1023).join("")).toEqual("1111111111");
+    expect(combination(10, choices, 1022).join("")).toEqual("0111111111");
+    expect(combination(10, choices, 512).join("")).toEqual("0000000001");
+    expect(combination(10, choices, 511).join("")).toEqual("1111111110");
+    // NOTICE the relationship to binary numbers is reverse: significance
+    // increases (left to right) not right to left
+    // to align with our visualization of array indies from left to right
+    expect(combination(10, choices, 0b1111100000).join("")).toEqual(
+      "0000011111",
+    );
+    expect(combination(10, choices, 0b0110110110).join("")).toEqual(
+      "0110110110",
+    );
+    expect(combination(10, choices, 3).join("")).toEqual("1100000000");
+    expect(combination(10, choices, 3).join("")).toEqual("1100000000");
+    assertThrows(() => combination(10, choices, 1024));
+    assertThrows(() => combination(10, choices, -1));
+  });
+});
+
+describe("combining functions and applying", () => {
+  const add = (a: number, b: number) => a + b;
+  const mul = (a: number, b: number) => a * b;
+  const choices: IntOp[] = [add, mul];
+  it("works with any kind of choice (a.k.a. 'digit')", () => {
+    expect(combination(2, choices, 0)).toEqual([add, add]);
+    expect(combination(2, choices, 1)).toEqual([mul, add]);
+    expect(combination(2, choices, 2)).toEqual([add, mul]);
+    expect(combination(2, choices, 3)).toEqual([mul, mul]);
+  });
+
+  it("we can apply those combinations to reduce an array", () => {});
+});
