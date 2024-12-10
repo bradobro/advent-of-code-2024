a9849ce0030508603caa77f17d1666864ed3b703
Brad Olson <brad.olson@movedbylight.com>
2024-12-10 08:12:58 -0500

    begin: stub day 10 and get data

--- deno/cli.ts
+++ deno/cli.ts
@@ -1,11 +1,12 @@
 // import { Day06 } from "./day_06.ts";
 // import { Day07 } from "./day_07.ts";
 // import { Day08 } from "./day_08.ts";
-import { Day09 } from "./day_9.ts";
+// import { Day09 } from "./day_09.ts";
+import { Day10 } from "./day_10.ts";
 
 export async function cli() {
-  console.log(`Running ${Deno.args}`);
-  const puz = new Day09();
+  // console.log(`Running ${Deno.args}`);
+  const puz = new Day10();
   const result = await puz.solve();
   console.log(result);
 }
