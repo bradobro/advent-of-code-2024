74580c0101b2524962e7b88b0201ecbda9d1d2c6
Brad Olson <brad.olson@movedbylight.com>
2025-01-03 21:59:55 -0500

    feat: solve day 17a

---
+++ deno2024/src/day_17_model.ts
@@ -0,0 +1,203 @@
+export interface State17 {
+  a: number;
+  b: number;
+  c: number;
+}
+
+export type Program17 = number[];
+
+export class Cpu17 {
+  trace = false;
+  pc = 0;
+  state: State17;
+
+  constructor(readonly state0: State17, readonly program: Program17) {
+    this.state = { ...state0 };
+  }
+
+  reset() {
+    this.pc = 0;
+    this.state = { ...this.state0 };
+  }
+
+  readonly instructionDescriptions = [
+    "adv0 a = a div 2**combo",
+    "bxl1 b ^= lit",
+    "bst2 b = combo % 8",
+    "jnz3 jump to lit if a != 0",
+    "bxc4 b ^= c (pc++)",
+    "out5 print(combo % 8)",
+    "bdv6 b = a div 2 ** combo",
+    "cdv7 c = a div 2 ** combo",
+  ];
+
+  // The adv instruction (opcode 0) performs division. The numerator is the
+  // value in the A register. The denominator is found by raising 2 to the power
+  // of the instruction's combo operand. (So, an operand of 2 would divide A by
+  // 4 (2^2); an operand of 5 would divide A by 2^B.) The result of the division
+  // operation is truncated to an integer and then written to the A register.
+  adv() {
+    const [_, combo] = this.getOperand();
+    if (this.trace) console.debug({ combo });
+    this.state.a = Math.trunc(this.state.a / 2 ** combo);
+    return "";
+  }
+
+  // PAUSE: possibly an alternative unusual solution exists
+
+  // The bxl instruction (opcode 1) calculates the bitwise XOR of register B and
+  // the instruction's literal operand, then stores the result in register B.
+  bxl() {
+    const [lit, _] = this.getOperand();
+    if (this.trace) console.debug({ lit });
+    this.state.b ^= lit;
+    return "";
+  }
+
+  // The bst instruction (opcode 2) calculates the value of its combo operand
+  // modulo 8 (thereby keeping only its lowest 3 bits), then writes that value
+  // to the B register.
+  bst() {
+    const [_, combo] = this.getOperand();
+    if (this.trace) console.debug({ combo });
+    this.state.b = combo % 8;
+    return "";
+  }
+
+  // The jnz instruction (opcode 3) does nothing if the A register is 0.
+  // However, if the A register is not zero, it jumps by setting the instruction
+  // pointer to the value of its literal operand; if this instruction jumps, the
+  // instruction pointer is not increased by 2 after this instruction.
+  jnz() {
+    const [lit, _] = this.getOperand();
+    if (this.state.a != 0) {
+      if (this.trace) console.debug({ lit });
+      this.pc = lit;
+    }
+    return "";
+  }
+
+  // The bxc instruction (opcode 4) calculates the bitwise XOR of register B and
+  // register C, then stores the result in register B. (For legacy reasons, this
+  // instruction reads an operand but ignores it.)
+  bxc() {
+    if (this.trace) console.debug(`skipping operand ${this.program[this.pc]}`);
+    this.pc++;
+    this.state.b ^= this.state.c;
+    return "";
+  }
+
+  // The out instruction (opcode 5) calculates the value of its combo operand
+  // modulo 8, then outputs that value. (If a program outputs multiple values,
+  // they are separated by commas.)
+  out() {
+    const [_, combo] = this.getOperand();
+    const output = (combo % 8).toString();
+    if (this.trace) console.debug({ combo, output });
+    return output;
+  }
+
+  // The bdv instruction (opcode 6) works exactly like the adv instruction
+  // except that the result is stored in the B register. (The numerator is still
+  // read from the A register.)
+  bdv() {
+    const [_, combo] = this.getOperand();
+    if (this.trace) console.debug({ combo });
+    this.state.b = Math.trunc(this.state.a / 2 ** combo);
+    return "";
+  }
+
+  // The cdv instruction (opcode 7) works exactly like the adv instruction
+  // except that the result is stored in the C register. (The numerator is still
+  // read from the A register.)
+  cdv() {
+    const [_, combo] = this.getOperand();
+    if (this.trace) console.debug({ combo });
+    this.state.c = Math.trunc(this.state.a / 2 ** combo);
+
+    return "";
+  }
+
+  readonly instructions = [
+    this.adv.bind(this),
+    this.bxl.bind(this),
+    this.bst.bind(this),
+    this.jnz.bind(this),
+    this.bxc.bind(this),
+    this.out.bind(this),
+    this.bdv.bind(this),
+    this.cdv.bind(this),
+  ];
+
+  getInstruction(opcode: number) {
+    const instruction = this.instructions[opcode];
+    if (this.trace) {
+      console.debug(
+        `${instruction.name.slice(6)}${opcode}(${this.program[this.pc + 1]}): ${
+          this.instructionDescriptions[opcode]
+        }`,
+      );
+    }
+    this.pc++;
+    if (instruction) return instruction;
+    throw new Error(`unknown opcode "${opcode} at pc=${this.pc}`);
+  }
+
+  getOperand(): [number, number] {
+    if (this.pc >= this.program.length) {
+      throw new Error(`could not get operand at pc=${this.pc}`);
+    }
+    const literal = this.program[this.pc];
+    this.pc++;
+    if (literal < 0) throw new Error(`invalid operand ${literal}@${this.pc}`);
+    if (literal < 4) return [literal, literal];
+    if (literal === 4) return [literal, this.state.a];
+    if (literal === 5) return [literal, this.state.b];
+    if (literal === 6) return [literal, this.state.c];
+    if (literal === 7) return [literal, -Infinity];
+    throw new Error(`invalid operand ${literal}@${this.pc}`);
+  }
+
+  *step() {
+    let step = 0;
+    if (this.trace) {
+      console.debug("==========");
+      console.debug({ program: this.program });
+      console.debug({ ...this.snapshot() });
+      console.debug("----------");
+    }
+    while (this.pc < this.program.length) {
+      step++;
+      const instruction = this.getInstruction(this.program[this.pc]);
+      yield instruction();
+      if (this.trace) {
+        console.debug({ afterstep: step, ...this.snapshot() });
+      }
+    }
+  }
+
+  run() {
+    this.reset();
+    const output = Array.from(this.step()).filter((a) => a).join(",");
+    return output;
+  }
+
+  snapshot() {
+    const { a, b, c } = this.state;
+    return {
+      a,
+      b,
+      c,
+      pc: this.pc,
+      snippet: this.program.slice(this.pc, this.pc + 4),
+    };
+  }
+}
+
+export function getInput17a() {
+  return new Cpu17({
+    a: 64854237,
+    b: 0,
+    c: 0,
+  }, [2, 4, 1, 1, 7, 5, 1, 5, 4, 0, 5, 5, 0, 3, 3, 0]);
+}
---
+++ deno2024/src/day_17_test.ts
@@ -0,0 +1,70 @@
+import { describe, it } from "jsr:@std/testing/bdd";
+import { expect } from "jsr:@std/expect";
+import { Cpu17, getInput17a } from "./day_17_model.ts";
+
+function example1() {
+  return new Cpu17(
+    { a: 729, b: 0, c: 0 },
+    [0, 1, 5, 4, 3, 0],
+  );
+}
+
+describe("problem a", () => {
+  it("handles example 1", () => {
+    const eg = new Cpu17({ a: 0, b: 0, c: 9 }, [2, 6]);
+    // eg.trace = true;
+    eg.run();
+    expect(eg.snapshot().b).toEqual(1);
+  });
+  it("handles example 2", () => {
+    const eg = new Cpu17({ a: 10, b: 0, c: 0 }, [5, 0, 5, 1, 5, 4]);
+    // eg.trace = true;
+    const output = eg.run();
+    expect(output).toEqual("0,1,2");
+  });
+  it("handles example 3", () => {
+    const eg = new Cpu17({ a: 2024, b: 0, c: 0 }, [0, 1, 5, 4, 3, 0]);
+    // eg.trace = true;
+    const output = eg.run();
+    expect(output).toEqual("4,2,5,6,7,7,7,7,3,1,0");
+    expect(eg.snapshot().a).toEqual(0);
+  });
+  it("handles example 4", () => {
+    const eg = new Cpu17({ a: 0, b: 29, c: 0 }, [1, 7]);
+    // eg.trace = true;
+    eg.run();
+    expect(eg.snapshot().b).toEqual(26);
+  });
+  it("handles example 5", () => {
+    const eg = new Cpu17({ a: 0, b: 2024, c: 43690 }, [4, 0]);
+    eg.trace = true;
+    eg.run();
+    expect(eg.snapshot().b).toEqual(44354);
+  });
+  // const out: string[] = [];
+  it("handles the big example", () => {
+    const eg = example1();
+    // eg.trace = true;
+    const output = eg.run();
+    expect(output).toEqual("4,6,3,5,6,3,5,2,1,0");
+  });
+  it("solves part a", () => {
+    const eg = getInput17a();
+    // eg.trace = true;
+    const output = eg.run();
+    console.debug({ output });
+  });
+});
+
+describe("part 2", () => {
+  it("demos a copying program", () => {
+    const program = [0, 3, 5, 4, 3, 0];
+    const eg = new Cpu17({
+      a: 117440,
+      b: 0,
+      c: 0,
+    }, program);
+    const output = eg.run();
+    expect(output).toEqual(program);
+  });
+});
